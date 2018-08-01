ce3ae619599a0c19eb04226f7105aeeafacc50f7
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-08-01 22:55:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,9 +22,7 @@ client.on("guildMemberAdd", member => {
       var jimp = require('jimp')
       
       const mi = ['./img/w1.jpg','./img/w2.jpg','./img/w3.jpg','./img/w4.jpg','./img/w5.jpg','./img/w6.jpg','./img/w7.jpg','./img/w8.jpg','./img/w9.jpg','./img/w10.jpg','./img/w11.jpg','./img/w12.jpg','./img/w13.jpg','./img/w14.jpg','./img/w15.jpg','./img/w16.jpg','./img/w17.jpg','./img/w18.jpg','./img/w19.jpg','./img/w20.jpg']; //يمكن ضيف '/img/w3.png','/img/w4.png'...
-      
-     
-      
+  
               let Image = Canvas.Image,
                   canvas = new Canvas(401, 202),
                   ctx = canvas.getContext('2d');
@@ -34,105 +32,49 @@ client.on("guildMemberAdd", member => {
               ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
               ctx.shadowOffsetY = 2;
               ctx.shadowBlur = 2;
-              fs.readFile(`${mi[Math.floor(Math.random() * mi.length)]}`, function (err, Background) {
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
                   if (err) return console.log(err)
                   let BG = Canvas.Image;
                   let ground = new Image;
                   ground.src = Background;
                   ctx.drawImage(ground, 0, 0, 401, 202);
-      
+
       })
-      
-                     let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                        if (err) return console.log(err);
-      
-                              //AVATARً
+
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+
+                              
                               let Avatar = Canvas.Image;
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.drawImage(ava, 152, 27, 95, 95);
-                            //member number..
-                        ctx.font = '13px Arial';
-                        ctx.fontSize = '20px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText(`انت العضو رقم  ${member.guild.memberCount} ! `, 152 , 27);
-    
-                                                      //name
+
+                                                      //wl
                               ctx.font = '20px Arial Bold';
-                              ctx.fontSize = '20px';
+                              ctx.fontSize = '15px';
                               ctx.fillStyle = "#FFFFFF";
                               ctx.textAlign = "center";
-                                                         ctx.fillText(`${member.user.username}`, 200, 154);
-                             
-                             //server name
+                                                         ctx.fillText(member.user.username, 200, 154);
+
+                              //NAME
                               ctx.font = '20px Arial';
                               ctx.fontSize = '28px';
                               ctx.fillStyle = "#FFFFFF";
                               ctx.textAlign = "center";
-   ctx.fillText(` WELCOME TO ATY ${member.guild.name} ` , 200, 190);
-   
-   
+                                    ctx.fillText(`انت العضو رقم${member.guild.memberCount} `
+                              , 200, 190);
 
-    welcomer.sendFile(canvas.toBuffer())
-      
-      });
-      });
-      });
+ welcomer.sendFile(canvas.toBuffer())
 
 
-const moment = require('moment');
-client.on("guildMemberAdd", member => {
-let welcomer = member.guild.channels.find("name","chat");
-      if(!welcomer) return;
-      if(welcomer) {
-         moment.locale('ar-ly');
-         var h = member.user;
-        let norelden = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(h.avatarURL)
-        .setAuthor(h.username,h.avatarURL)
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
-         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:norelden});          
-               
- 
-      }
+
+      })
+      })
       });
 
-
-
-client.on('guildMemberRemove', member => {
-    var embed = new Discord.RichEmbed()
-    .setAuthor(member.user.username, member.user.avatarURL)
-    .setThumbnail(member.user.avatarURL)
-    .setTitle(`خرج عضو`)
-    .setDescription(`**__طلع آو تم طرده من قبل الآدارة__** 👏`)
-    .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-    .setColor('RED')
-    .setFooter(`H Bot`, '')
-
-var channel =member.guild.channels.find('name', 'chat')
-if (!channel) return;
-channel.send({embed : embed});
-});
-
-
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S7Q,~THE~BEST`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
-
-// THIS  MUST  BE  THIS  WAY
+    
 client.login(process.env.BOT_TOKEN);
